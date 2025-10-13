a53a71fc909d2f5409d4ba349a407048f93c7ae8
Vadim Popov <vadimpopov@externalworkforce.google.com>
2025-10-13 15:06:50 +0200

    Fiixing tests again

--- test/integration_test.cc
+++ test/integration_test.cc
@@ -183,6 +183,7 @@ TEST_F(IntegrationTest, ServerRestart) {
   }
   
   // Start new server
+  server_ready_ = false;
   server_thread_ = std::thread([this]() {
     const std::string server_address("localhost:0");
     service_ = std::make_unique<GreeterServiceImpl>();
@@ -196,9 +197,9 @@ TEST_F(IntegrationTest, ServerRestart) {
     server_port_ = port;
     server_ready_ = true;
     
-    std::unique_lock<std::mutex> lock(server_mutex_);
-    server_cv_.wait(lock, [this] { return server_ready_; });
-    
+    // std::unique_lock<std::mutex> lock(server_mutex_);
+    // server_cv_.wait(lock, [this] { return server_ready_; });
+    server_cv_.notify_all();
     server_->Wait();
   });
   
